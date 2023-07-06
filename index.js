505bf048e694cbe30a78c1700dbe29c7b4e4755f
Varsha <varsha981sahu@gmail.com>
2023-07-06 20:51:20 +0530

    setup static files access for layouts and pages

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ const express=require('express');
 const app = express();
 //port num
 const  port=8000;
+const expressLayouts = require('express-ejs-layouts');
+app.use(express.static('./assets'));
 
+app.use(expressLayouts);
+//extract style and scripts from sub pages into the layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 //use express Router
 app.use('/',require('./routes/index'));
 
+//set up view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 //listening the port
 app.listen(port,function(err){
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 //doesn't make everytime new instance its refers to same express in index.js
 const express = require('express');
 const router = express.Router();
-const homeCotroller=require('../controllers/home_controller')
+const homeController=require('../controllers/home_controller')
 console.log('router loaded');
 
 //use home controller
-router.get('/',homeCotroller.home);
+router.get('/',homeController.home);
+router.use('/users',require('./users'));
 module.exports = router;
 
