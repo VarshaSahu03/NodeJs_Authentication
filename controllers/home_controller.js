68736f985c8b14ad352fe2313065dfc513668c3e
Varsha <varsha981sahu@gmail.com>
2023-07-10 09:15:39 +0530

    display post and created user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,25 @@
+const Post = require('../models/post');
 module.exports.home = function(req, res){
-    console.log(req.cookies);
+    // console.log(req.cookies);
     // res.cookie('user_id', 25);
-    return res.render('home', {
-        title: "Home"
+
+    // Post.find({},function(err,posts){
+
+    //     return res.render('home', {
+    //         title: "Home",
+    //         posts:posts
+    //     });
+    // });
+    //querrry
+    //populate the user for each post
+    Post.find({}).populate('user').exec(function(err,posts){
+
+        return res.render('home', {
+            title: "Home",
+            posts:posts
+        });
     });
+    
 }
 
 // module.exports.actionName = function(req, res){}
